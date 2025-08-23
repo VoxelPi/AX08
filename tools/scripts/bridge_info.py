d237066c222001922a9a5a1d5ac5e6ed6010aa8a
Peter Smek <smek.peter@gmail.com>
2025-08-23 21:40:50 +0200

    Dont loop in bridge info tool

--- tools/scripts/bridge_info.py
+++ tools/scripts/bridge_info.py
@@ -1,6 +1,5 @@
 import hashlib
 import serial
-import time
 
 # --- Configure the port ---
 ser = serial.Serial(
@@ -30,52 +29,48 @@ packet_message = b"\1" # Send the id of the query info packet.
 send_packet(packet_message)
 
 try:
-    while True:
-        data = ser.read(2048+32+4)  # Read up to 1024 bytes
-        if data:
-            hash_part = data[0:32]
-            size_part = data[32:36]
-            data_part = data[36:]
+    data = ser.read(2048+32+4)  # Read up to 1024 bytes
+    if data:
+        hash_part = data[0:32]
+        size_part = data[32:36]
+        data_part = data[36:]
 
-            size = int.from_bytes(size_part, byteorder="little", signed=False)
-            data_part = data_part[:size]
+        size = int.from_bytes(size_part, byteorder="little", signed=False)
+        data_part = data_part[:size]
 
-            m = hashlib.sha256()
-            m.update(size_part + data_part)
-            hash_valid = hash_part == m.digest()
+        m = hashlib.sha256()
+        m.update(size_part + data_part)
+        hash_valid = hash_part == m.digest()
 
+        if not hash_valid:
             print(f"Received packet, size={size}, hash={hash_valid}: \"{data_part.hex()}\"")
-            if not hash_valid:
-                print(f"RECEIVED: {hash_part.hex()}")
-                print(f"SHOULD BE: {m.digest().hex()}")
+            print(f"RECEIVED: {hash_part.hex()}")
+            print(f"SHOULD BE: {m.digest().hex()}")
 
-            buffer = data_part
-            packet_type_id = int.from_bytes(buffer[:1], byteorder="little", signed=False)
-            buffer = buffer[1:]
-            if packet_type_id == 1:
-                protocol_version = int.from_bytes(buffer[:4], byteorder="little", signed=False)
-                buffer = buffer[4:]
+        buffer = data_part
+        packet_type_id = int.from_bytes(buffer[:1], byteorder="little", signed=False)
+        buffer = buffer[1:]
+        if packet_type_id == 1:
+            protocol_version = int.from_bytes(buffer[:4], byteorder="little", signed=False)
+            buffer = buffer[4:]
 
-                version_length = int.from_bytes(buffer[:2], byteorder="little", signed=False)
-                buffer = buffer[2:]
-                version = buffer[:version_length].decode("utf-8")
-                buffer = buffer[version_length:]
+            version_length = int.from_bytes(buffer[:2], byteorder="little", signed=False)
+            buffer = buffer[2:]
+            version = buffer[:version_length].decode("utf-8")
+            buffer = buffer[version_length:]
 
-                git_version_length = int.from_bytes(buffer[:2], byteorder="little", signed=False)
-                buffer = buffer[2:]
-                git_version = buffer[:git_version_length].decode("utf-8")
-                buffer = buffer[git_version_length:]
+            git_version_length = int.from_bytes(buffer[:2], byteorder="little", signed=False)
+            buffer = buffer[2:]
+            git_version = buffer[:git_version_length].decode("utf-8")
+            buffer = buffer[git_version_length:]
 
-                print(f"  - protocol version: {protocol_version}")
-                print(f"  - version:          \"{version}\"")
-                print(f"  - git version:      \"{git_version}\"")
-            else:
-                print(f"  - unknown packet type {packet_type_id}")
+            print("Bridge information:")
+            print(f"  - protocol version: {protocol_version}")
+            print(f"  - version:          \"{version}\"")
+            print(f"  - git version:      \"{git_version}\"")
+        else:
+            print(f"Received packet, size={size}, hash={hash_valid}: \"{data_part.hex()}\"")
+            print(f"  - unknown packet type {packet_type_id}")
 
-        send_packet(packet_message)
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("\nExiting.")
 finally:
     ser.close()
