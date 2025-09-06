4f35bbefc8f411064513c92ffd935f1b4cb078d5
Peter Smek <smek.peter@gmail.com>
2025-09-06 23:44:16 +0200

    Improve memory unit throughput

--- firmware/ax08-bridge/src/ax08/memory/memory_unit.c
+++ firmware/ax08-bridge/src/ax08/memory/memory_unit.c
@@ -12,7 +12,7 @@
 
 #include "program.h"
 
-#define AX08_MEMORY_BAUD 115200
+#define AX08_MEMORY_BAUD 2000000
 
 // Pin mapping
 const unsigned int PINS_MEMORY_INSTRUCTION_ADDRESS[2] = {14, 15};
@@ -34,16 +34,9 @@ typedef union byteint16_t {
     uint8_t bytes[2];
 } byteint16;
 
-typedef union byteint32_t {
-    uint32_t word;
-    uint8_t bytes[4];
-} byteint32;
-
 volatile byteint16 instruction_address_word;
-volatile byteint32 instruction_word;
 volatile byteint16 data_address_word;
 volatile uint8_t data_word_in;
-volatile uint8_t data_word_out;
 
 uint8_t memory_data[0x10000];
 
@@ -58,22 +51,6 @@ void memory_init_instruction_pio() {
         // Configure the pio state machine.
         pio_sm_claim(pio0, sm);
         uart_tx_program_init(pio0, sm, tx_program_offset, pin, AX08_MEMORY_BAUD);
-
-        // Configure a dma channel to continuously copy the value from the variable to the pio fifo.
-        int dma_chan = dma_claim_unused_channel(true);
-        dma_channel_config dma_chan_con = dma_channel_get_default_config(dma_chan);
-        channel_config_set_transfer_data_size(&dma_chan_con, DMA_SIZE_8);
-        channel_config_set_read_increment(&dma_chan_con, false);
-        channel_config_set_write_increment(&dma_chan_con, false);
-        channel_config_set_dreq(&dma_chan_con, pio_get_dreq(pio0, sm, true));
-        dma_channel_configure(
-            dma_chan,
-            &dma_chan_con,
-            &pio0->txf[sm],
-            instruction_word.bytes + i_part,
-            dma_encode_endless_transfer_count(),
-            true
-        );
     }
 }
 
@@ -160,38 +137,25 @@ void memory_init_data_pio() {
             true
         );
     }
-    
+
     {
         unsigned int sm = 1;
 
         // Configure a pio state machine for the data out byte.
         pio_sm_claim(pio2, sm);
         uart_tx_program_init(pio2, sm, tx_program_offset, PIN_MEMORY_DATA_OUT, AX08_MEMORY_BAUD);
-
-        // Configure a dma channel to continuously copy the value from the variable to the pio fifo.
-        int dma_chan = dma_claim_unused_channel(true);
-        dma_channel_config dma_chan_con = dma_channel_get_default_config(dma_chan);
-        channel_config_set_transfer_data_size(&dma_chan_con, DMA_SIZE_8);
-        channel_config_set_read_increment(&dma_chan_con, false);
-        channel_config_set_write_increment(&dma_chan_con, false);
-        channel_config_set_dreq(&dma_chan_con, pio_get_dreq(pio2, sm, true));
-        dma_channel_configure(
-            dma_chan,
-            &dma_chan_con,
-            &pio2->txf[sm],
-            &data_word_out,
-            dma_encode_endless_transfer_count(),
-            true
-        );
     }
 }
 
 void ax08_memory_core1_entry() {
     bool is_prev_hold = false;
+    uint32_t previous_instruction_word = 0x00000000;
+    uint8_t previous_data_word = 0x00;
+
     while (true) {
+        // Handle data write.
         bool is_write = !gpio_get(PIN_MEMORY_OPCODE);
         bool is_hold = gpio_get(PIN_MEMORY_HOLD);
-
         if (is_write) {
             if (is_hold && !is_prev_hold) {
                 memory_data[data_address_word.word] = data_word_in;
@@ -201,8 +165,22 @@ void ax08_memory_core1_entry() {
             is_prev_hold = false;
         }
 
-        data_word_out = memory_data[data_address_word.word];
-        instruction_word.word = ax08_program_instructions[instruction_address_word.word];
+        // Update data word.
+        uint8_t data_word = memory_data[data_address_word.word];
+        if (data_word != previous_data_word) {
+            pio_sm_put(pio1, 1, data_word);
+            previous_data_word = data_word;
+        }
+
+        // Update instruction word.
+        uint32_t instruction_word = ax08_program_instructions[instruction_address_word.word];
+        if (instruction_word != previous_instruction_word) {
+            pio_sm_put(pio0, 0, (instruction_word >>  0) & 0xFF);
+            pio_sm_put(pio0, 1, (instruction_word >>  8) & 0xFF);
+            pio_sm_put(pio0, 2, (instruction_word >> 16) & 0xFF);
+            pio_sm_put(pio0, 3, (instruction_word >> 24) & 0xFF);
+            previous_instruction_word = instruction_word;
+        }
     }
 }
 
@@ -212,10 +190,12 @@ void ax08_memory_init() {
     memory_init_address_pio();
     memory_init_data_pio();
 
+    // Configure extra pins.
     gpio_init(PIN_MEMORY_OPCODE);
-    gpio_set_dir(PIN_MEMORY_OPCODE, GPIO_IN); 
+    gpio_set_dir(PIN_MEMORY_OPCODE, GPIO_IN);
     gpio_init(PIN_MEMORY_HOLD);
-    gpio_set_dir(PIN_MEMORY_HOLD, GPIO_IN); 
+    gpio_set_dir(PIN_MEMORY_HOLD, GPIO_IN);
 
+    // Launch memory unit main method on core1.
     multicore_launch_core1(ax08_memory_core1_entry);
 }
