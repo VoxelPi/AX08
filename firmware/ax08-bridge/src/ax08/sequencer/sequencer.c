a6dae1bb81b23166409896d23f469c8f2cd90b99
Peter Smek <smek.peter@gmail.com>
2025-09-14 15:40:19 +0200

    Assign flow control pins on bridge to sequencer port

--- firmware/ax08-bridge/src/ax08/sequencer/sequencer.c
+++ firmware/ax08-bridge/src/ax08/sequencer/sequencer.c
@@ -12,4 +12,6 @@ void ax08_sequencer_init() {
     // Set the TX and RX pins by using the function select on the GPIO.
     gpio_set_function(4, GPIO_FUNC_UART); // UART1 TX
     gpio_set_function(5, GPIO_FUNC_UART); // UART1 RX
+    gpio_set_function(6, GPIO_FUNC_UART); // UART1 CTS
+    gpio_set_function(7, GPIO_FUNC_UART); // UART1 RTS
 }
