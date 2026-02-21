4cc076d64317adb29f03330c188f5bbc5f2b99a8
Peter Smek <smek.peter@gmail.com>
2026-02-21 23:16:22 +0100

    Start work on command module

---
+++ firmware/ax08-sequencer/src/command.c
@@ -0,0 +1,121 @@
+#include "command.h"
+
+#include <pic.h>
+#include <xc.h>
+
+#include "state.h"
+
+volatile uint8_t cmd_rx_buffer[BRIDGE_UART_BUFFER_SIZE];
+uint8_t i_cmd_rx_read;
+volatile uint8_t i_cmd_rx_write;
+volatile bool poll_command;
+
+typedef enum ax08_command_rx_state {
+    AX08_CMD_RX_STATE_ID = 0,
+    AX08_CMD_RX_STATE_LENGTH = 1,
+    AX08_CMD_RX_STATE_PAYLOAD = 2,
+} ax08_command_rx_state_t;
+ax08_command_rx_state_t command_rx_state; // Command RX state.
+uint8_t command_id = 0;
+uint8_t command_payload_length = 0;
+uint8_t command_payload_start = 0;
+uint8_t remaining_payload = 0;            // How much of the commands payload is remaining, only valid in the PAYLOAD RX state.
+
+#define AX08_COMMAND_TOOGLE_DEBUG_MODE 0x01
+#define AX08_COMMAND_RUN_INSTRUCTION 0x02
+#define AX08_COMMAND_RUN_STEP 0x03
+#define AX08_COMMAND_SELECT_TIMER 0x04
+#define AX08_COMMAND_UPLOAD_TIMER_CONFIG 0x40
+
+void ax08_seq_command_handle(void);
+
+void ax08_seq_command_init() {
+    // Configure UART
+    PIE1bits.RCIE = true;   // Enable UART RX interrupts.
+    RCSTAbits.SPEN = true;  // Enable serial port. (Configures RX and TX pins as serial port pins)
+    TXSTAbits.SYNC = false; // Asynchronous mode.
+    RCSTAbits.CREN = true;  // Enable receive.
+    TXSTAbits.TXEN = true;  // Enable transmit.
+}
+
+void ax08_seq_command_update(void) {
+    while (i_cmd_rx_read != i_cmd_rx_write) {
+
+        switch (command_rx_state) {
+        case AX08_CMD_RX_STATE_ID:
+            // Received command id.
+            command_rx_state = AX08_CMD_RX_STATE_LENGTH;
+            command_id = cmd_rx_buffer[i_cmd_rx_read];
+            ++i_cmd_rx_read;
+            break;
+
+        case AX08_CMD_RX_STATE_LENGTH:
+            // Receive payload length.
+            command_rx_state = AX08_CMD_RX_STATE_PAYLOAD;
+            command_payload_length = cmd_rx_buffer[i_cmd_rx_read];
+            remaining_payload = cmd_rx_buffer[i_cmd_rx_read];
+            ++i_cmd_rx_read;
+            command_payload_start = i_cmd_rx_read;
+
+            // Handle commands without payload.
+            if (command_payload_length == 0) {
+                // Handle command.
+                command_rx_state = AX08_CMD_RX_STATE_ID;
+                ax08_seq_command_handle();
+            }
+
+            break;
+
+        case AX08_CMD_RX_STATE_PAYLOAD:
+            // Receive payload element.
+            --remaining_payload;
+            ++i_cmd_rx_read;
+
+            // Check if command is fully received.
+            if (remaining_payload == 0) {
+                // Handle command.
+                command_rx_state = AX08_CMD_RX_STATE_ID;
+                ax08_seq_command_handle();
+            }
+            break;
+        }
+    }
+}
+
+void ax08_seq_command_handle(void) {
+    // Disable interrupts.
+    di();
+    switch (command_id) {
+        case AX08_COMMAND_TOOGLE_DEBUG_MODE:
+            if (enabled) {
+                ax08_seq_action_toggle_debug_mode();
+            } else {
+                ax08_seq_action_reset();
+            }
+            break;
+
+        case AX08_COMMAND_RUN_INSTRUCTION:
+            ax08_seq_action_run_instruction();
+            break;
+
+        case AX08_COMMAND_RUN_STEP:
+            ax08_seq_action_run_step();
+            break;
+
+        case AX08_COMMAND_SELECT_TIMER:
+            if (command_payload_length < 1) {
+                // Missing clock index argument.
+                break;
+            }
+            ax08_seq_action_select_timer(cmd_rx_buffer[(uint8_t)(command_payload_start + 0)]);
+            break;
+
+        case AX08_COMMAND_UPLOAD_TIMER_CONFIG:
+            break;
+
+        default:
+            // Do nothing on invalid command, read next command.
+            break;
+    }
+    ei();
+}
---
+++ firmware/ax08-sequencer/src/command.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+    Variables related to the UART bridge link.
+*/
+#define BRIDGE_UART_BUFFER_SIZE 256 // The size of the receive buffer.
+
+extern volatile uint8_t cmd_rx_buffer[BRIDGE_UART_BUFFER_SIZE]; // The receive buffer.
+extern volatile uint8_t i_cmd_rx_write;                         // The location of the write pointer.
+extern volatile bool poll_command;                              // If there is a command event to be processed.
+
+/**
+    Initializes the hardware used by the command module.
+*/
+void ax08_seq_command_init(void);
+
+/**
+    Updates the command module state.
+*/
+void ax08_seq_command_update(void);
